0df89a9d38b6efb181d9a39e23a681fcc4b1ba5c
Alexander Schlieden <alex.schlieden@gmail.com>
2023-02-01 20:29:51 +0100

    update 1 store.js

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,4 +1,3 @@
-
 import { configureStore } from '@reduxjs/toolkit';
 import contactsSlice from './contacts/contacts.slice';
 import filterSlice from './contacts/filter.slice';
@@ -9,3 +8,46 @@ export const store = configureStore({
     filter: filterSlice,
   },
 });
+
+// import { combineReducers, configureStore } from '@reduxjs/toolkit';
+
+// import {
+//   persistStore,
+//   persistReducer,
+//   FLUSH,
+//   REHYDRATE,
+//   PAUSE,
+//   PERSIST,
+//   PURGE,
+//   REGISTER,
+// } from 'redux-persist';
+
+// import storage from 'redux-persist/lib/storage';
+// import contactsSlice from './contacts.slice';
+// import filterSlice from './filter.slice';
+
+// const persistConfig = {
+//   key: 'contacts',
+//   storage,
+//   blacklist: ['filter'],
+// };
+
+// const rootReducer = combineReducers({
+//   contacts: contactsSlice,
+//   filter: filterSlice,
+// });
+
+// const persistedReducer = persistReducer(persistConfig, rootReducer);
+
+// const store = configureStore({
+//   reducer: persistedReducer,
+//   middleware: getDefaultMiddleware =>
+//     getDefaultMiddleware({
+//       serializableCheck: {
+//         ignoredActions: [FLUSH, REHYDRATE, PAUSE, PERSIST, PURGE, REGISTER],
+//       },
+//     }),
+// });
+// const persistor = persistStore(store);
+
+// export { store, persistor };
