db2b789ff4247d9177721b8bfa9d9d6cfb591ad1
Alexander Schlieden <alex.schlieden@gmail.com>
2023-02-06 18:36:14 +0100

    bug fix

--- src/index.js
+++ src/index.js
@@ -1,16 +1,16 @@
 import React from 'react';
-import { PersistGate } from 'redux-persist/integration/react'
+import { PersistGate } from 'redux-persist/integration/react';
 import ReactDOM from 'react-dom/client';
 import { BrowserRouter } from 'react-router-dom';
 import { App } from 'components/App';
 import './index.css';
-import { Provider } from 'react-redux'
-import { store, persistor } from './redux/store'
+import { Provider } from 'react-redux';
+import { store, persistor } from './redux/store';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <Provider store={store}>
     <PersistGate loading={null} persistor={persistor}>
-      <BrowserRouter>
+      <BrowserRouter basename="/goit-react-hw-08-phonebook">
         <App />
       </BrowserRouter>
     </PersistGate>
